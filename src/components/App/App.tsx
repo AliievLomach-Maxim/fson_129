a419183c9781886d7b8a530727c033a89b760593
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2026-09-02 21:30:52 +0300

    +

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,92 +1,144 @@
-import CatsInfo from '../CatsInfo/CatsInfo'
-import catsData from '../../cats.json'
+import { useState } from 'react'
+import Counter from '../Counter/Counter'
+import TotalValue from '../TotalValue/TotalValue'
 
-// const cat1 = {
-//   id: 1,
-//   name: 'Mr. Whiskerson',
-//   age: 3,
-//   email: 'whiskerson@catmail.com',
-//   image: 'https://cataas.com/cat/says/Hello',
-//   available: true,
-// }
-
-// const cat2 = {
-//   id: 2,
-//   name: 'Sir Pounce-a-lot',
-//   age: 5,
-//   email: 'pouncealot@catmail.com',
-//   image: 'https://cataas.com/cat/says/Meow',
-//   available: false,
-// }
+interface CounterState {
+  a: number
+  b: number
+  c: number
+}
 
 const App = () => {
+  const [counter, setCounter] = useState<CounterState>({ a: 0, b: 0, c: 0 })
+  // const [value,sv] = useState<number>(0)
+  // const handleClickA = () => {
+  //   setCounter({ ...counter, a: counter.a + 1 })
+  // }
+  // const handleClickB = () => {
+  //   setCounter({ ...counter, b: counter.b + 1 })
+  // }
+  // const handleClickC = () => {
+  //   setCounter({ ...counter, c: counter.c + 1 })
+  // }
+  const handleClick = (key: keyof CounterState) => {
+    setCounter({ ...counter, [key]: counter[key] + 1 })
+  }
+
+  const total = counter.a + counter.b + counter.c
+
+  console.log('counter', counter)
   return (
     <div>
-      <header>Header</header>
-      <main>
-        {/* <CatsInfo catData={cat1} extraInfo='test' />
-        <hr />
-        <CatsInfo catData={cat2} /> */}
-        {catsData.map((cat) => (
-          <CatsInfo catData={cat} extraInfo={cat.id > 3 ? 'test' : undefined} key={cat.id} />
-        ))}
-        <hr />
-        render
-        <br />
-        {/* {[
-          <CatsInfo catData={cat1} extraInfo='test' />,
-          <CatsInfo catData={cat1} extraInfo='test' />,
-          <CatsInfo catData={cat1} extraInfo='test' />,
-        ]} */}
-        {/* {true}
-        {false}
-        {undefined}
-        {null}
-        {''}
-        {0}
-        {NaN} */}
-      </main>
-      <footer>Footer</footer>
+      <Counter updateFn={() => handleClick('a')} value={counter.a} />
+      <hr />
+      <Counter updateFn={() => handleClick('b')} value={counter.b} />
+      <hr />
+      <Counter updateFn={() => handleClick('c')} value={counter.c} />
+      <hr />
+      <h2>Total: {total}</h2>
+      {/* <TotalValue obj={counter} /> */}
+      {/* <button onClick={handleClick}>Counter : {counter}</button>
+      <button onClick={handleClick}>Counter : {counter}</button> */}
     </div>
   )
 }
 
 export default App
 
-// const getName = (obj) => {
-//   const user = obj.user
-//   const extrainfo = obj.extrainfo
-
-//   return `${user.name} + ${extrainfo}`
+// const handleClick = (customValue: number) => {
+//   console.log(customValue + 2)
 // }
-// getName({ user: { name: 'Alex' }, extrainfo: 'asd' })
-// getName('qwe', {})
-// getName()
-// getName()
-// getName()
-// getName()
-// getName()
-// getName()
-// getName()
-// getName()
-// getName()
-// getName()
-// getName()
-// getName()
+// const button = document.querySelector('button')
+// button.addEvent('click', (event)=>handleClick(1))
 
-// const getName = (props) => {
-//   return props.user.name
-// }
-// const data = { user: { name: 'Alex' } }
-// getName(data)
-// const user = { name: 'Alex' }
-// getName(user)
+// import { useState } from 'react'
+// // let v = true
+// const App = () => {
+//   const [value, setValue] = useState(true)
 
-// const SomeComponent = () => {
+//   const handleClick = () => {
+//     setValue(!value)
+//   }
+
 //   return (
-//     <>
-//       <p></p>
-//       <p></p>
-//     </>
+//     <div>
+//       <button onClick={handleClick}>Toggle</button>
+//       <hr />
+//       {value && <p>Hello!!</p>}
+//     </div>
 //   )
 // }
+
+// export default App
+
+// import { useState } from 'react'
+
+// // hook > useSomth...
+// // const [value, fn] = useState() > let counterValue;
+// // const [value, fn] = useState(0) > let counterValue = 0
+// const App = () => {
+//   const [counterValue, setCounterValue] = useState(0)
+//   const handleCounter = () => {
+//     // counterValue = counterValue + 1
+//     setCounterValue(counterValue + 1) // counterValue = 1
+//     // setCounterValue(counterValue + 1) // counterValue = 1
+//     // setCounterValue(counterValue + 1) // counterValue = 1
+//     console.log('counterValue', counterValue) // 0
+//   }
+//   console.log('counterValue', counterValue) // 0 > 1
+
+//   return (
+//     <div>
+//       <button onClick={handleCounter}> Counter {counterValue}</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// const App = () => {
+//   // ts..
+//   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+//     console.log(event)
+//   }
+//   return (
+//     <div>
+//       <button onClick={handleClick}>Click me...</button>
+//       <button
+//         onClick={(event) => {
+//           console.log(event)
+//         }}
+//       >
+//         Click me...
+//       </button>
+
+//       <input
+//         type='text'
+//         onChange={(e) => {
+//           console.log('e from input', e)
+//         }}
+//       />
+//     </div>
+//   )
+// }
+
+// export default App
+
+// // 'input'
+// // const button = document.querySelector('')
+// // const handleClick = (event) => {}
+// // button.addEvent('click', (event) => {})
+// // button.addEvent('click', handleClick)
+
+// const someFn2 = () => {
+//   const b = 2
+// }
+
+// const someFn = () => {
+//   someFn2()
+//   console.log(2 + b)
+// }
+
+// let user = { name: 'Alex', age: 42 }
+// user = { ...user, name: 'Bob' }
+// user > { name: 'Bob', age: 42 }
