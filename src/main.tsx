73b1a6beb877d68ad32ea913480d7635ba13da04
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2026-09-09 21:01:05 +0300

    +

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App/App'
 
-createRoot(document.getElementById('root') as HTMLDivElement).render(<App />)
+// StrictMode > App() mounting > testing > unmount > App() mounting
+
+createRoot(document.getElementById('root') as HTMLDivElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
