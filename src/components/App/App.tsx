dcec9fad1bbc4a94c9ee361162299b6575ad9ac4
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2026-09-14 21:16:40 +0300

    +

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,75 +1,69 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import { searchArticles } from '../../services/api'
+import ArticleList from '../ArticleList/ArticleList'
+import SearchForm from '../SearchForm/SearchForm'
 import { useState } from 'react'
-import Sidebar from '../Sidebar/Sidebar'
+import ResponsivePagination from 'react-responsive-pagination'
+import 'react-responsive-pagination/themes/classic-light-dark.css'
 
 const App = () => {
-  const [isShowSidebar, setIsShowSidebar] = useState(false)
+  const [searchValue, setSearchValue] = useState('')
+  const [page, setPage] = useState(1)
 
-  const showSidebar = () => setIsShowSidebar(true)
-  const hideSidebar = () => setIsShowSidebar(false)
+  const handleSearch = (value: string) => {
+    setSearchValue(value)
+    setPage(1)
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['articles', searchValue, page],
+    queryFn: () => searchArticles(searchValue, page),
+    placeholderData: keepPreviousData,
+  })
 
   return (
     <div>
-      <h1>My super site</h1>
-      <button onClick={showSidebar}>open</button>
-      {isShowSidebar && <Sidebar onClose={hideSidebar} />}
+      <SearchForm isLoading={isLoading} handleSearch={handleSearch} />
+      {data && (
+        <>
+          <ArticleList articles={data.hits} />
+          <ResponsivePagination
+            current={page}
+            total={data.nbPages}
+            onPageChange={(newPage) => setPage(newPage)}
+          />
+        </>
+      )}
     </div>
   )
 }
 
 export default App
 
+// import { useQuery } from '@tanstack/react-query'
+// import { fetchPerson } from '../../services/swapiService'
 // import { useState } from 'react'
-// import Timer from '../Timer/Timer'
-
-// const App = () => {
-//   const [isShow, setIsShow] = useState(false)
-//   return (
-//     <div>
-//       <button onClick={() => setIsShow(!isShow)}>{isShow ? 'Hide' : 'Show'}</button>
-//       {isShow && <Timer />}
-//     </div>
-//   )
-// }
-
-// export default App
-// import { useEffect, useState } from 'react'
-// import { searchArticles } from '../../services/api'
 
 // const App = () => {
 //   const [counter, setCounter] = useState(0)
-//   const [articles, setArticles] = useState([])
 
-//   // setCounter(counter + 1)
-//   //
-
-//   useEffect(() => {
-//     // console.log('useEffect')
-//     // searchArticles('css').then((res) => setArticles(res))
-//     async function fetchData() {
-//       // You can await here
-//       const response = await searchArticles('css')
-//       setArticles(response)
-//       // ...
-//     }
-//     fetchData()
-//   }, [])
-//   // if(firstRender === true){callback()}else{return}
-
-//   // if(prevcounter !== counter){callback()}else{return}
-//   // useEffect(() => {
-//   //   console.log('counter', counter)
-//   // }, [counter])
-
-//   // console.log('render App')
+//   const { data, isLoading, isError } = useQuery({
+//     queryKey: ['persons', counter],
+//     queryFn: () => fetchPerson(counter),
+//     enabled: counter > 0,
+//   })
 
 //   return (
 //     <div>
-//       <button onClick={() => setCounter(counter + 1)}>{counter}</button>
-//       <pre>{JSON.stringify(articles, null, 2)}</pre>
+//       {isLoading && <h1>Loading...</h1>}
+//       {isError && <h1>Oops..</h1>}
+//       <button onClick={() => setCounter(counter - 1)}>-1</button>
+//       <h1>{counter}</h1>
+//       <button onClick={() => setCounter(counter + 1)}>+1</button>
+//       <pre>{JSON.stringify(data, null, 2)}</pre>
+//       {/* <h2>{data}</h2> */}
 //     </div>
 //   )
 // }
 
 // export default App
-
-// // App() > setCounter(counter + 1) > re-render (App()) > setCounter(counter + 1)> re-render...
